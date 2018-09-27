7a5b4852f67d2aa2ea204efcb798f3c6c277a2e1
iamRaziAkram <37903391+iamRaziAkram@users.noreply.github.com>
2018-09-27 18:47:30 +0530

    newip

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -10,7 +10,7 @@ class Controller extends Component {
   constructor()
   {
     super();
-    this.baseUrl = "http://52.221.183.14:8080/api/v1/";
+    this.baseUrl = "http://13.229.71.228:8080/api/v1/";
   }
   render(){
     return(
