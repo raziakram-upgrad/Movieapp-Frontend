89e0abb3afa0a660a675c797a1f67d0b6e732465
iamRaziAkram <37903391+iamRaziAkram@users.noreply.github.com>
2018-11-05 18:58:49 +0530

    NewIPs

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -10,7 +10,7 @@ class Controller extends Component {
   constructor()
   {
     super();
-    this.baseUrl = "http://18.136.102.246:8080/api/v1/";
+    this.baseUrl = "http://18.136.195.105:8080/api/v1/";
   }
   render(){
     return(
